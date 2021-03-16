b17714ed3c0d3638c1fcbd64c9840e65bec7da24
Dawid Gola <dawidgola@MacBook-Pro-Dawid.local>
2021-03-17 00:46:29 +0100

    add-backend-fn-read-file

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -2,7 +2,7 @@ import Navigation from 'components/Navigation';
 
 export default function Layout({ children }) {
   return (
-    <div className="p-10 font-mono">
+    <div className="p-10 font-mono max-w-screen-xl mx-auto">
       <Navigation />
       <main className="bg-gray-200 p-5">{children}</main>
       <footer className="bg-blue-400 text-gray-100 text-center">{new Date().getFullYear()}</footer>
